9f1b23317f5de498773f66ac5746e833e7484bdf
Rohan Karamel <rak218@scarletmail.rutgers.edu>
2025-03-24 18:01:43 -0400

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rakdcolon.github.io/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
